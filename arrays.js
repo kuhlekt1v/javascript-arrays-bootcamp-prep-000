dad53b55a3204d3e7ac70bfdb9cff4f0a1c9a86c
Cody Sheridan <csheridan994@gmail.com>
2018-04-11 15:13:26 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars, element) {
+  return ["foo", ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element) {
+  return chocolateBars.unshift("foo");
+}
+
+function addElementToEndOfArray(chocolateBars, element) {
+  return [...chocolateBars, "foo"];
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, element) {
+  return chocolateBars.push("foo");
+}
+
+function accessElementInArray (chocolateBars, index) {
+  return chocolateBars[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (chocolateBars, element) {
+  return chocolateBars.shift();
+}
+
+function removeElementFromBeginningOfArray (chocolateBars, element) {
+  return chocolateBars.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray (chocolateBars, element) {
+  return chocolateBars.pop();
+}
+
+function removeElementFromEndOfArray(chocolateBars) {
+  return chocolateBars.slice(0,chocolateBars.length - 1);
+}
